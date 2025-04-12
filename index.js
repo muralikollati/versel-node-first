0019a81c54273231201df2db7e8d5e19a5766968
Murali <murali@Muralis-MacBook-Pro.local>
2025-04-12 19:38:14 +0530

    eight

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
 // Export as a serverless function
 module.exports = (req, res) => {
   app(req, res);
