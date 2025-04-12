3af963daf7b83f9571897a7c44183127fa1fbeda
Murali <murali@Muralis-MacBook-Pro.local>
2025-04-12 19:10:59 +0530

    fifth

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// api/index.js
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
+// Export as a serverless function
+module.exports = (req, res) => {
+  app(req, res);
+};
