47b4ce62a3130041fbe3dbaf01437b5f5a4301e8
Murali <murali@Muralis-MacBook-Pro.local>
2025-04-12 19:07:54 +0530

    versel

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// server.js (for local only)
 const express = require("express");
-
 const app = express();
-const PORT = 4000
-app.get("/", (req, res) => {
-  res.status(200).json("✅ Local server working now!");
-});
 
-app.get("/test", (req, res) => {
-  res.status(200).json("✅ this is test route");
-});
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
